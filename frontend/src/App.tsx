43e027d6ef476ea0495ef16b793ace3311254d5d
Aline Sanches <sanches.aline6@gmail.com>
2021-09-09 11:50:23 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h2 className="text-primary">Olá Mundo!</h2>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
