e1d917ae1f4b6bb5becc6005988641c87a1964b7
Aline Sanches <sanches.aline6@gmail.com>
2021-09-11 06:53:20 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -8,35 +9,30 @@ type ChartData = {
     series: number[];
 }
 
-function DonutChart(){
+function DonutChart() {
 
-    // FORMA ERRADA
-    let chartData : ChartData = { labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabesl = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabesl = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabesl, series: mySeries}
-            console.log(chartData);
-        });
-    /* const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    } */
-    
+                setChartData({ labels: myLabesl, series: mySeries });
+            });
+    }, []);
+
     const options = {
         legend: {
             show: true
         }
     }
-    
-    return(
-        <Chart 
-            options={{...options, labels:chartData.labels}}
+
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
